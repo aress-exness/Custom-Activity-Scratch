01b2d0c43bd9128c2549c5b3e3fcd8ed710cf943
aress-exness <78411334+aress-exness@users.noreply.github.com>
2021-05-04 18:20:24 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -90,15 +90,15 @@ define([
         var postcardURLValue = $('#postcard-url').val();
         var postcardTextValue = $('#postcard-text').val();
 
-       // console.log("dataExtensionName"+dataExtensionName);
+       console.log("dataExtensionName"+PushyAPIDataDictionaryV2);
         
         payload['arguments'].execute.inArguments = [
 		{"tokens": authTokens},
 		 { "Title": "{{Event.943F1CE9-2D1B-4F77-AFD9-9F3FE44DD234.Title}}"},
 		   { "Data": "{{Contact.Attribute.PushyAPIDataDictionaryV2.Data}}"},
-		   {"DeviceToken": "{{Contact.Attribute.PushyAPIDataDictionaryV2.DeviceToken}}"}
-            
-        ];
+		   {"DeviceToken": "{{Contact.Attribute.PushyAPIDataDictionaryV2.DeviceToken}}"
+		   "tokens": authTokens
+		   }];
         
         
         payload['metaData'].isConfigured = true;
